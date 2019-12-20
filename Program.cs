fd021bbde3a598d7224ac51b04afc28c106af90f
Seamus Feider-Sullivan <sfeidersullivan@tesla.com>
2019-12-20 10:10:29 -0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace starwars_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -- Problem --
+            // Using the Starwars API, build a console app that creates a short bio for a given character
+            
+            // -- Example --
+            // Input: "Darth Vader"
+            // Output: "Darth Vader, born 41.9BBY, has appeared in 4 films."
+
+            // -- Resources --
+            // Starwars API: https://swapi.co/
+
+            // -- Running --
+            // In the Terminal: `dotnet run "Darth Vader"`
+
+            Console.WriteLine("A long time ago in a galaxy far, far away....");
+        }
+    }
+}
